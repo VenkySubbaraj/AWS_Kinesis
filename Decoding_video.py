fa5ad3582413e0d9f6e5fce404b4ef248b0b2682
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-09-08 10:00:36 +0530

    Update Decoding_video.py

--- Decoding_video.py
+++ Decoding_video.py
@@ -11,7 +11,7 @@ def lambda_handler(event, handler):
     image = base64.b64encode(urlopen("https://dockercontainer2.s3.ap-south-1.amazonaws.com/SampleVideo_720x480_5mb.mp4").read())
     print(image)
     
-    s3_client = boto3.client('s3', aws_access_key_id="AKIA3LN3YN5C3VW7UWFQ", aws_secret_access_key="3NTQ+TXIcj1E4/5WuLStouLFyBeucTKIVbLE3u+R", region_name="ap-south-1")
+    s3_client = boto3.client('s3')
     s3_client.put_object(ACL='public-read',Body=image, Bucket='dockercontainer2', Key='sample')
     
     print("INNSIDE")
